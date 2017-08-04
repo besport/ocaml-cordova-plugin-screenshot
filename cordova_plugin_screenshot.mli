e3c54a6a2cc275b13ab2abd6ea6ca99ff810568b
Ke <Ke.Douglas.Wang@hotmail.com>
2017-08-04 18:13:24 +0200

    modify the signature of plugin

--- cordova_plugin_screenshot.mli
+++ cordova_plugin_screenshot.mli
@@ -1,24 +1,33 @@
 (* -------------------------------------------------------------------------- *)
+module Response_uri : sig
+  type t
+  val uri : t -> string [@@js.get "URI"]
+end
+
+module Response_file : sig
+  type t
+  val filePath : t -> string [@@js.get "filePath"]
+end
+
 val save :
-  ?callback: (string -> unit) ->
-  ?format: string             ->
-  ?quality: int               ->
-  filename: string            ->
+  ?callback: (string -> Response_file.t ->  unit) ->
+  ?format: string                                 ->
+  ?quality: int                                   ->
+  filename: string                                ->
   unit
 [@@js.global "navigator.screenshot.save"]
 
 val uri :
-  ?callback: (string -> unit) ->
-  ?quality: int               ->
-  unit                        ->
+  ?callback: (string -> Response_uri.t ->  unit) ->
+  ?quality: int                                  ->
+  unit                                           ->
   unit
 [@@js.global "navigator.screenshot.URI"]
 
 val uri_sync : 
-  ?callback: (string -> unit) ->
-  ?quality: int               ->
-  unit                        ->
+  ?callback: (string -> Response_uri.t -> unit) ->
+  ?quality: int                                 ->
+  unit                                          ->
   unit
 [@@js.global "navigator.screenshot.URISync"]
-
 (* -------------------------------------------------------------------------- *)
